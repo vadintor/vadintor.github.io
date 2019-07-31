43c1e0bd60227d94703bcc60759f4acec2cf1828
Torbjörn Vading <torbjorn.vading@nivapro.com>
2019-07-31 22:24:15 +0200

    deploy

---
+++ precache-manifest.baf71f697955364f963fe9fc720f8561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3be32c2c1a9b20c03e02",
+    "url": "/js/chunk-vendors.8c2acddc.js"
+  },
+  {
+    "revision": "42c003d58c66fd96a92d",
+    "url": "/js/app.fc48277e.js"
+  },
+  {
+    "revision": "e99d8089a1972a480529041bb882518c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "42c003d58c66fd96a92d",
+    "url": "/css/app.e24eaafb.css"
+  }
+];
